ed503038f6ecceb6003cc939960fd802e82a2860
remiss <bhanpuriyabhudev03@gmail.com>
2025-03-13 22:07:37 +0530

    removed routes for admin

--- week-8/index.js
+++ week-8/index.js
@@ -23,24 +23,3 @@ app.post("/purchase",async function(req,res){
 app.get("/courses",async function(req,res){
     
 })
-
-
-app.post("/admin-signup",async function(res,res){
-
-})
-
-app.post("/admin-login",async function(res,res){
-
-})
-
-app.post("/create-course",async function(req,res){
-
-})
-
-app.post("/delete-course",async function(req,res){
-
-})
-
-app.post("/add-content",async function(req,res){
-
-})
