2cd929f6032cd31490649f9f6fdba6e8fd8ffc59
remiss <bhanpuriyabhudev03@gmail.com>
2025-03-15 15:34:24 +0530

    updated schemas

--- week-8/db.js
+++ week-8/db.js
@@ -1,27 +1,48 @@
-const zod = require("zod")
 const mongoose = require("mongoose")
 
-const schema  = mongoose.schema
-const ObjectId = mongoose.ObjectId
+console.log("connect to db")
+mongoose.connect("mongodb+srv://bhudev03:bhudev123@cluster0.frdi2.mongodb.net/course-selling-app")
 
-const Users = new schema({
+
+const schema  = mongoose.Schema
+const ObjectId = mongoose.Types.ObjectId;
+
+const userSchema = new schema({
     email : {type :String , unique : true},
-    password : zod.string.min(3).max(12)
+    password : String,
+    firstName : String,
+    lastName : String
 })
 
-const Admin = new schema({
-    adminEmail : zod.string.min(3).max(20),
-    password : zod.string.min(3).max(12)
+const adminSchema = new schema({
+    email : {type :String , unique : true},
+    password : String,
+    firstName : String,
+    lastName : String
 })
 
-const Course = new schema({
-    description : zod.string,
-    price : zod.price,
-    courseId : ObjectId
+const courseSchema = new schema({
+    title : String,
+    description : String,
+    price : Number,
+    imageUrl : String,
+    creatorId : ObjectId
 })
 
-const Purchase = new schema({
-    courseName : zod.string,
-    noOfPurchases : zod.number,
-    userId : ObjectId
+const purchaseSchema = new schema({
+    userId : ObjectId,
+    courseId : ObjectId,
+    noOfPurchases : Number
 })
+
+const userModel = mongoose.model("user",userSchema);
+const adminModel = mongoose.model("admin",adminSchema);
+const courseModel = mongoose.model("course",courseSchema);
+const purchaseModel = mongoose.model("purchase",purchaseSchema);
+
+module.exports = {
+    userModel,
+    adminModel,
+    courseModel,
+    purchaseModel
+}
