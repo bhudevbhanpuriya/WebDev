60a29d66be200e212c7871160308f4dfe59bb084
remiss <bhanpuriyabhudev03@gmail.com>
2025-03-15 15:41:22 +0530

    connected database

--- week-8/index.js
+++ week-8/index.js
@@ -4,6 +4,8 @@ const JWT_SECRET = "strangerthings"
 const zod = require("zod")
 const mongoose = require("mongoose")
 
+
+
 const {userRouter} = require("./routes/user");
 const {adminRouter} = require("./routes/admin");
 const {courseRouter} = require("./routes/course");
@@ -16,5 +18,12 @@ app.use("/api/v1/user" , userRouter);
 app.use("/api/v1/admin" , adminRouter);
 app.use("/api/v1/courses" , courseRouter)
 
+async function main(){
+    console.log("connect to database");
+    await mongoose.connect("mongodb+srv://bhudev03:bhudev123@cluster0.frdi2.mongodb.net/course-selling-app")
+    app.listen(3000)
+    console.log("listening on port 3000");
+    
+}
 
-app.listen(3000)
+main()
