4a0de477c6d06f9d59bd2b4b728c3e9c04dab854
remiss <bhanpuriyabhudev03@gmail.com>
2025-03-31 14:33:36 +0530

    added useState

---
+++ week-9/useState/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+
+function App() {
+
+  return (
+    <div>
+      <h1>React Hooks</h1>
+
+      <Counter />
+    </div>
+  )
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  function increaseCount() {
+    setCount(count + 1);
+  }
+
+  function decreaseCount() {
+    setCount(count - 1);
+  }
+
+  function resetCount() {
+    setCount(0);
+  }
+
+  return (
+    <div>
+      <h1>{count}</h1>
+
+      <button onClick={increaseCount}>Increase Count</button>
+
+      <button onClick={decreaseCount}>Decrease Count</button>
+      
+      <button onClick={resetCount}>Reset Count</button>
+    </div>
+  )
+}
+
+export default App;
