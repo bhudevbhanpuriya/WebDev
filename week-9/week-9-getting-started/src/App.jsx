3e45c88f588aa919ab14d135a6270f985a29654f
remiss <bhanpuriyabhudev03@gmail.com>
2025-03-31 14:39:57 +0530

    added useEffect

--- week-9/week-9-getting-started/src/App.jsx
+++ week-9/week-9-getting-started/src/App.jsx
@@ -1,42 +1,79 @@
-import { useState } from 'react';
+// import { useState } from 'react';
+
+// function App() {
+
+//   return (
+//     <div>
+//       <h1>React Hooks</h1>
+
+//       <Counter />
+//     </div>
+//   )
+// }
+
+// function Counter() {
+//   const [count, setCount] = useState(0);
+
+//   function increaseCount() {
+//     setCount(count + 1);
+//   }
+
+//   function decreaseCount() {
+//     setCount(count - 1);
+//   }
+
+//   function resetCount() {
+//     setCount(0);
+//   }
+
+//   return (
+//     <div>
+//       <h1>{count}</h1>
+
+//       <button onClick={increaseCount}>Increase Count</button>
+
+//       <button onClick={decreaseCount}>Decrease Count</button>
+      
+//       <button onClick={resetCount}>Reset Count</button>
+//     </div>
+//   )
+// }
+
+// export default App;
+
+
+
+
+
+import { useState, useEffect } from "react";
 
 function App() {
-
-  return (
-    <div>
-      <h1>React Hooks</h1>
-
-      <Counter />
-    </div>
-  )
+    return (
+        <div>
+            <h1>React Hooks</h1>
+            <Counter />
+        </div>
+    );
 }
 
 function Counter() {
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
-  function increaseCount() {
-    setCount(count + 1);
-  }
+    useEffect(() => {
+        setInterval(() => {
+            setCount(function (count) {
+                return count + 1;
+            });
+        }, 1000);
 
-  function decreaseCount() {
-    setCount(count - 1);
-  }
+        console.log("Mounted");
+    }, []); 
 
-  function resetCount() {
-    setCount(0);
-  }
-
-  return (
-    <div>
-      <h1>{count}</h1>
-
-      <button onClick={increaseCount}>Increase Count</button>
-
-      <button onClick={decreaseCount}>Decrease Count</button>
-      
-      <button onClick={resetCount}>Reset Count</button>
-    </div>
-  )
+    return (
+        <div>
+            <h1>{count}</h1>
+        </div>
+    );
 }
 
 export default App;
