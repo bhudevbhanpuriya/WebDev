6d8018ed955ade008008e953f335d5e3fe78561f
remiss <bhanpuriyabhudev03@gmail.com>
2025-03-31 14:30:32 +0530

    added react hook

--- week-9/week-9-getting-started/src/App.jsx
+++ week-9/week-9-getting-started/src/App.jsx
@@ -1,67 +1,42 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from 'react';
 
 function App() {
+
   return (
     <div>
-      <ToggleMessage></ToggleMessage>
-      {/* <Counter></Counter> */}
+      <h1>React Hooks</h1>
+
+      <Counter />
     </div>
   )
 }
 
-const ToggleMessage = () => {
-  const [isVisible , setIsVisible] = useState(false);
+function Counter() {
+  const [count, setCount] = useState(0);
 
-  return (
-    <div>
-      <button onClick = {() => setIsVisible(!isVisible)}>
-        Toggle Message
-      </button>
-      {isVisible && <p>this message is conditionally rendered</p>}
-    </div>
-  )
-}
-
-
-function Counter(){
-  const [count , setCount] = useState(0);
-
-
-  useEffect(function(){
-    setInterval(function increaseCount(){
-      setCount(count => count+1)
-    }, 1000)
-  },[])
-
-  // function increaseCount(){
-  //   setCount(count => count+1)
-  // }
-  
-
-  function increaseCount(){
-    setCount(count+1);
+  function increaseCount() {
+    setCount(count + 1);
   }
 
-  // function decreaseCount(){
-  //   setCount(count-1);
-  // }
+  function decreaseCount() {
+    setCount(count - 1);
+  }
 
-  // function resetCount(){
-  //   setCount(count-count);
-  // }
+  function resetCount() {
+    setCount(0);
+  }
 
   return (
     <div>
-       <h1 id="text">{count}</h1>
-       <button onClick={increaseCount}>Increase Count</button>
-       {/* <button onClick={decreaseCount}>Decrease Count</button> 
-        <button onClick={resetCount}>Reset Count</button>  */}
+      <h1>{count}</h1>
+
+      <button onClick={increaseCount}>Increase Count</button>
+
+      <button onClick={decreaseCount}>Decrease Count</button>
+      
+      <button onClick={resetCount}>Reset Count</button>
     </div>
   )
 }
 
-export default App
+export default App;
